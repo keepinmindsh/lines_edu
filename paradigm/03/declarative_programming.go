6e32c0b9d24a5094bbad0ac113ba54eb82e28714
Jeong Seung Hwa <keepinmindsh@gmail.com>
2023-04-16 18:04:51 +0900

    Update declarative_programming.go

--- paradigm/03/declarative_programming.go
+++ paradigm/03/declarative_programming.go
@@ -1,6 +1,35 @@
-package main 
+package main
+
+import (
+    "fmt"
+)
+
+type predicateFunc func(int) bool
+
+func filter(numbers []int, predicate predicateFunc) []int {
+    var result []int
+    for _, num := range numbers {
+        if predicate(num) {
+            result = append(result, num)
+        }
+    }
+    return result
+}
+
+func isEven(number int) bool {
+    return number%2 == 0
+}
+
+func isPositive(number int) bool {
+    return number > 0
+}
 
 func main() {
-  
-  
+    numbers := []int{-3, -2, -1, 0, 1, 2, 3}
+
+    evenNumbers := filter(numbers, isEven)
+    fmt.Printf("Even numbers: %v\n", evenNumbers)
+
+    positiveNumbers := filter(numbers, isPositive)
+    fmt.Printf("Positive numbers: %v\n", positiveNumbers)
 }
