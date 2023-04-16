db22092d57fa43a85c829fb2994e53de9e39799b
Jeong Seung Hwa <keepinmindsh@gmail.com>
2023-04-16 19:47:00 +0900

    Object Oriented Programming

--- paradigm/03/procedure_programming.go
+++ paradigm/03/procedure_programming.go
@@ -7,9 +7,21 @@ type Point struct {
 	Y int
 }
 
+type Shape interface {
+	getArea() int
+}
+
 type Square struct {
 	TopLeft Point
 	Side    int
+}
+
+func (s *Square) getArea() int {
+	return s.Side * s.TopLeft.X * s.TopLeft.Y
+}
+
+func NewSquare() Shape {
+	return &Square{}
 }
 
 type Rectangle struct {
@@ -18,26 +30,42 @@ type Rectangle struct {
 	Width   int
 }
 
+func (r *Rectangle) getArea() int {
+	return r.TopLeft.X * r.TopLeft.Y * r.Height * r.Width
+}
+
+func NewRectangle() Shape {
+	return &Rectangle{}
+}
+
 type Circle struct {
 	center Point
 	radius int
 }
 
-func main() {
-	area := getArea("CIRCLE")
-
-	fmt.Println(area)
+func (c *Circle) getArea() int {
+	return c.radius * c.center.X * c.center.Y
 }
 
-func getArea(shape string) int {
-	if shape == "CIRCLE" {
-		circle := &Circle{}
-		return circle.radius * circle.center.Y * circle.center.X
-	} else if shape == "SQUARE" {
-		square := &Square{}
-		return square.TopLeft.X * square.TopLeft.Y * square.Side
-	} else {
-		rectangle := &Rectangle{}
-		return rectangle.Width * rectangle.Height * rectangle.TopLeft.X * rectangle.TopLeft.Y
+func NewCircle() Shape {
+	return &Circle{}
+}
+
+func main() {
+	area := FactoryShape("CIRCLE")
+
+	fmt.Println(area.getArea())
+}
+
+func FactoryShape(shape string) Shape {
+	switch shape {
+	case "CIRCLE":
+		return NewCircle()
+	case "SQUARE":
+		return NewSquare()
+	case "RECTANGLE":
+		return NewRectangle()
+	default:
+		panic("error")
 	}
 }
