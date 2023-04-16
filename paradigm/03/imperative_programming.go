fc37c8a12cc2c36d366fd5b6c3cb87f8c16f71b9
Jeong Seung Hwa <keepinmindsh@gmail.com>
2023-04-16 17:44:33 +0900

    Update imperative_programming.go

--- paradigm/03/imperative_programming.go
+++ paradigm/03/imperative_programming.go
@@ -5,18 +5,21 @@ import (
 )
 
 func factorial(n int) int {
-	result := 1
-	for i := 1; i <= n; i++ {
-		result = result * i
-	}
-	return result
+    result := 1
+
+    for i := 1; i <= n; i++ {
+        result = result * i
+    }
+
+    return result
 }
 
 func main(){
-  var number int
-	fmt.Print("Enter a number to calculate its factorial: ")
-	fmt.Scan(&number)
+    var number int
+    fmt.Print("Enter a number to calculate its factorial: ")
 
-	fact := factorial(number)
-	fmt.Printf("Factorial of %d is: %d\n", number, fact)
+    fmt.Scan(&number)
+
+    fact := factorial(number)
+    fmt.Printf("Factorial of %d is: %d\n", number, fact)
 }
